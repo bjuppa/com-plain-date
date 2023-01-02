8498b2c137a655412c83366be0ff1ce88839fd7c
Björn Nilsved <nilsved@gmail.com>
2023-01-02 10:20:40 +0100

    Use locale "en"

--- utils/splitDateTime.ts
+++ utils/splitDateTime.ts
@@ -2,11 +2,12 @@ import { PlainDate } from "../PlainDate.ts";
 
 export const splitDateTime = (timezone: string) => (instant?: Date) => {
   instant ??= new Date();
+  const locale = "en";
   const options = { timeZone: timezone };
   const plainDate = PlainDate.of({
-    year: instant.toLocaleDateString([], { ...options, year: "numeric" }),
-    month: instant.toLocaleDateString([], { ...options, month: "numeric" }),
-    day: instant.toLocaleDateString([], { ...options, day: "numeric" }),
+    year: instant.toLocaleDateString(locale, { ...options, year: "numeric" }),
+    month: instant.toLocaleDateString(locale, { ...options, month: "numeric" }),
+    day: instant.toLocaleDateString(locale, { ...options, day: "numeric" }),
   });
   return [plainDate];
 };
