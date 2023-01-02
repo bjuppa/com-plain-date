84d65b99a8e3a47d7d67f885817ac0bde8bdc0a5
Björn Nilsved <nilsved@gmail.com>
2023-01-02 09:58:16 +0100

    Remove todo that'd make things more complex

--- utils/splitDateTime.ts
+++ utils/splitDateTime.ts
@@ -2,7 +2,6 @@ import { PlainDate } from "../PlainDate.ts";
 
 export const splitDateTime = (timezone: string) => (date?: Date) => {
   date ??= new Date();
-  // TODO: use formatToParts() to extract parts in one go
   const options = { timeZone: timezone };
   const plainDate = PlainDate.of({
     year: date.toLocaleDateString([], { ...options, year: "numeric" }),
