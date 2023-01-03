dd9cbdab1c205f7adfc5d1b15e2a913bfc387603
Björn Nilsved <nilsved@gmail.com>
2023-01-03 10:56:58 +0100

    Make parameter name clearer

--- utils/intlParts.ts
+++ utils/intlParts.ts
@@ -1,8 +1,12 @@
 type PartsRecord = Partial<Record<Intl.DateTimeFormatPartTypes, string>>;
 
-export const intlParts = (idtf: Intl.DateTimeFormat) => (instant: Date) => {
-  return idtf.formatToParts(instant).reduce<PartsRecord>((acc, part) => {
-    acc[part.type] = part.value;
-    return acc;
-  }, {});
-};
+export const intlParts =
+  (intlDateTimeFormat: Intl.DateTimeFormat) => (instant: Date) => {
+    return intlDateTimeFormat.formatToParts(instant).reduce<PartsRecord>(
+      (acc, part) => {
+        acc[part.type] = part.value;
+        return acc;
+      },
+      {},
+    );
+  };
