15bf48ae079241a8e02bd579b720cdc067bc8df6
Björn Nilsved <nilsved@gmail.com>
2023-01-04 09:28:22 +0100

    Add time parts to new Date object

---
+++ utils/addTime.ts
@@ -0,0 +1,15 @@
+import { SloppyPlainTime } from "../support/sloppy-types.ts";
+import { milliseconds } from "./milliseconds.ts";
+
+export const addTime = ({
+  hour = 0,
+  minute = 0,
+  second = 0,
+  millisecond = 0,
+}: SloppyPlainTime) =>
+(instant: Date) => {
+  return new Date(
+    instant.valueOf() +
+      milliseconds({ hour, minute, second, millisecond }),
+  );
+};
---
+++ utils/addTime_test.ts
@@ -0,0 +1,12 @@
+import { addTime } from "./addTime.ts";
+import { assert, assertEquals } from "../testing/asserts.ts";
+
+Deno.test("returns new Date with added time parts", () => {
+  const originalInstant = new Date(0);
+  const newInstant = addTime({ hour: 1, minute: 1, second: 1, millisecond: 1 })(
+    originalInstant,
+  );
+
+  assert(originalInstant !== newInstant, "Objects are the same");
+  assertEquals(newInstant.toISOString(), "1970-01-01T01:01:01.001Z");
+});
