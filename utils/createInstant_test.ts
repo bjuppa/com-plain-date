98fe118b26190149c7c1397139d588f378ef512b
Björn Nilsved <nilsved@gmail.com>
2023-01-05 10:18:25 +0100

    Test DST transitions

--- utils/createInstant_test.ts
+++ utils/createInstant_test.ts
@@ -2,6 +2,7 @@ import { createInstant } from "./createInstant.ts";
 import { assert, assertEquals, assertInstanceOf } from "../testing/asserts.ts";
 
 Deno.test("returned date object is valid", () => {
+  // Stockholm is 1 hour ahead of UTC when not in DST
   const instant = createInstant("Europe/Stockholm")({
     year: 2022,
     month: 1,
@@ -17,4 +18,99 @@ Deno.test("returned date object is valid", () => {
   assertEquals(instant.toISOString(), "2022-01-01T22:59:59.999Z");
 });
 
-//TODO: test DST transitions
+Deno.test("during DST", () => {
+  // Stockholm is 2 hours ahead of UTC during DST
+  const instant = createInstant("Europe/Stockholm")({
+    year: 2022,
+    month: 6,
+    day: 1,
+    hour: 12,
+  });
+
+  assertEquals(instant.toISOString(), "2022-06-01T10:00:00.000Z");
+});
+
+Deno.test("just before going into DST", () => {
+  // Stockholm went into DST 2022-03-27 02:00 becoming UTC+2
+  const instant = createInstant("Europe/Stockholm")({
+    year: 2022,
+    month: 3,
+    day: 27,
+    hour: 1,
+    minute: 59,
+    second: 59,
+    millisecond: 999,
+  });
+
+  assertEquals(instant.toISOString(), "2022-03-27T00:59:59.999Z");
+});
+
+Deno.test("just after going into DST", () => {
+  // Stockholm went into DST 2022-03-27 02:00, skipping to 03:00 and becoming UTC+2
+  const instant = createInstant("Europe/Stockholm")({
+    year: 2022,
+    month: 3,
+    day: 27,
+    hour: 3,
+  });
+
+  assertEquals(instant.toISOString(), "2022-03-27T01:00:00.000Z");
+});
+
+Deno.test("moves ahead to after transition during during transition to DST", () => {
+  // Stockholm went into DST 2022-03-27 02:00, skipping to 03:00 and becoming UTC+2
+  // 2:30 doesn't exist during transition, so should skip forward to 3:30 in UTC+2
+  const instant = createInstant("Europe/Stockholm")({
+    year: 2022,
+    month: 3,
+    day: 27,
+    hour: 2,
+    minute: 30,
+  });
+
+  assertEquals(instant.toISOString(), "2022-03-27T01:30:00.000Z");
+});
+
+Deno.test("just before going out of DST", () => {
+  // Stockholm went out of DST 2022-10-30 03:00 repeating 02:00 and going back to UTC+1
+  const instant = createInstant("Europe/Stockholm")({
+    year: 2022,
+    month: 10,
+    day: 30,
+    hour: 1,
+    minute: 59,
+    second: 59,
+    millisecond: 999,
+  });
+
+  assertEquals(instant.toISOString(), "2022-10-29T23:59:59.999Z");
+});
+
+Deno.test("just after going out of DST", () => {
+  // Stockholm went out of DST 2022-10-30 03:00 repeating 02:00 and going back to UTC+1
+  const instant = createInstant("Europe/Stockholm")({
+    year: 2022,
+    month: 10,
+    day: 30,
+    hour: 3,
+    millisecond: 1,
+  });
+
+  assertEquals(instant.toISOString(), "2022-10-30T02:00:00.001Z");
+});
+
+Deno.test("uses the latter of repeated times during transition out of DST", () => {
+  // Stockholm went out of DST 2022-10-30 03:00 repeating 02:00 and going back to UTC+1
+  // Times between 2:00 and 3:00 exists twice during transition, so should use the latter
+  const instant = createInstant("Europe/Stockholm")({
+    year: 2022,
+    month: 10,
+    day: 30,
+    hour: 2,
+    minute: 59,
+    second: 59,
+    millisecond: 999,
+  });
+
+  assertEquals(instant.toISOString(), "2022-10-30T01:59:59.999Z");
+});
