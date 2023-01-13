b18d3a631559c7f2f88df6b8f60e3cd08a681059
Björn Nilsved <nilsved@gmail.com>
2023-01-13 17:58:41 +0100

    Check first parameter outside returned function

--- utils/firstWeekDay.ts
+++ utils/firstWeekDay.ts
@@ -3,16 +3,14 @@ import { PlainDateContract } from "../PlainDate.ts";
 import { addDays } from "./addDays.ts";
 import { weekDay } from "./weekDay.ts";
 
-export const firstWeekDay =
-  (targetWeekDay: number) => (plainDate: PlainDateContract) => {
-    if (targetWeekDay < 1 || targetWeekDay > 7) {
-      throw new TypeError(
-        `Target week day must be between 1 and 7: ${targetWeekDay}`,
-      );
-    }
-    return addDays(
-      (targetWeekDay - weekDay(plainDate) + DAYS_IN_WEEK) % DAYS_IN_WEEK,
-    )(
-      plainDate,
+export const firstWeekDay = (targetWeekDay: number) => {
+  if (targetWeekDay < 1 || targetWeekDay > 7) {
+    throw new TypeError(
+      `Target week day must be between 1 and 7: ${targetWeekDay}`,
     );
-  };
+  }
+  return (plainDate: PlainDateContract) =>
+    addDays(
+      (targetWeekDay - weekDay(plainDate) + DAYS_IN_WEEK) % DAYS_IN_WEEK,
+    )(plainDate);
+};
