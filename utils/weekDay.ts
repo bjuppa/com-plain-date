30a95641a0edffdcb566055efe483499d3258875
Björn Nilsved <nilsved@gmail.com>
2023-01-25 16:08:04 +0100

    Use const to clarify operation

--- utils/weekDay.ts
+++ utils/weekDay.ts
@@ -1,6 +1,9 @@
-import { WeekDayNumber } from "../constants.ts";
+import { DAYS_IN_WEEK, WeekDayNumber } from "../constants.ts";
 import { PlainDateContract } from "../PlainDate.ts";
 
 export const weekDay = (
   plainDate: PlainDateContract,
-) => ((plainDate.toUtcInstant().getUTCDay() + 6) % 7 + 1) as WeekDayNumber;
+) =>
+  (
+    (plainDate.toUtcInstant().getUTCDay() + 6) % DAYS_IN_WEEK + 1
+  ) as WeekDayNumber;
