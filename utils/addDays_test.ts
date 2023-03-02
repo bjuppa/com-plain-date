e95de37ef39a97770feed9776da63b688e7a5826
Björn Nilsved <nilsved@gmail.com>
2023-03-02 16:39:16 +0100

    Test parity with difference-in-days

--- utils/addDays_test.ts
+++ utils/addDays_test.ts
@@ -1,6 +1,7 @@
 import { addDays } from "./addDays.ts";
 import { PlainDate } from "../PlainDate.ts";
 import { assertEquals } from "../testing/asserts.ts";
+import { differenceInDays } from "./differenceInDays.ts";
 
 Deno.test("days can be added with positive argument", () => {
   const plainDate = PlainDate({ year: 2022, month: 1, day: 31 });
@@ -12,4 +13,11 @@ Deno.test("days can be subtracted with negative argument", () => {
   const plainDate = PlainDate({ year: 2022, month: 1, day: 1 });
 
   assertEquals(String(addDays(-2)(plainDate)), "2021-12-30");
+});
+
+Deno.test("has parity with difference-in-days", () => {
+  const plainDate = PlainDate({ year: 2022, month: 3, day: 31 });
+
+  assertEquals(differenceInDays(plainDate)(addDays(100)(plainDate)), 100);
+  assertEquals(differenceInDays(plainDate)(addDays(-100)(plainDate)), -100);
 });
