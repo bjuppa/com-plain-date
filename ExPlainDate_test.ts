b555316878256f846a379cc673305e58b96b2482
Björn Nilsved <nilsved@gmail.com>
2023-04-27 09:13:56 +0200

    Remove tests already covered in PlainDate test

--- ExPlainDate_test.ts
+++ ExPlainDate_test.ts
@@ -1,33 +1,11 @@
 import { ExPlainDate, ExtendedPlainDateContract } from "./ExPlainDate.ts";
 import { PlainDate } from "./PlainDate.ts";
-import {
-  assertEquals,
-  assertStringIncludes,
-  assertThrows,
-} from "./testing/asserts.ts";
+import { assertEquals, assertThrows } from "./testing/asserts.ts";
 
 Deno.test("factory accepts PlainDate", () => {
   const exPlainDate = ExPlainDate(PlainDate({ year: 2022, month: 2, day: 2 }));
 
   assertEquals(String(exPlainDate), "2022-02-02");
-});
-
-Deno.test("factory accepts number date parts", () => {
-  const exPlainDate = ExPlainDate({ year: 2022, month: 2, day: 2 });
-
-  assertEquals(String(exPlainDate), "2022-02-02");
-});
-
-Deno.test("factory accepts string date parts", () => {
-  const exPlainDate = ExPlainDate({ year: "2022", month: "02", day: "02" });
-
-  assertEquals(String(exPlainDate), "2022-02-02");
-});
-
-Deno.test("factory throws error when date is invalid", () => {
-  assertThrows(() => {
-    ExPlainDate({ year: NaN });
-  });
 });
 
 Deno.test("enumerable properties can not be set", async (t) => {
@@ -41,38 +19,6 @@ Deno.test("enumerable properties can not be set", async (t) => {
       });
     });
   }
-});
-
-Deno.test("main string representations are the same ISO standard string", () => {
-  const exPlainDate = ExPlainDate({ year: 2022, month: 2, day: 2 });
-  const iso = "2022-02-02";
-
-  assertEquals(exPlainDate.iso, iso);
-  assertEquals(exPlainDate.valueOf(), iso);
-  assertEquals(exPlainDate.toString(), iso);
-  assertEquals(exPlainDate.toJSON(), iso);
-  assertEquals(String(exPlainDate), iso);
-});
-
-Deno.test("up to 6-digit years can be represented in ISO string", () => {
-  const exPlainDate = ExPlainDate({ year: 100000, month: 1, day: 1 });
-
-  assertEquals(exPlainDate.iso, "+100000-01-01");
-});
-
-Deno.test("negative years can be represented in ISO string", () => {
-  const exPlainDate = ExPlainDate({ year: -1, month: 1, day: 1 });
-
-  assertEquals(exPlainDate.iso, "-000001-01-01");
-});
-
-Deno.test("can be localized", () => {
-  const exPlainDate = ExPlainDate({ year: 2020, month: 6, day: 13 });
-
-  assertEquals(
-    exPlainDate.toLocaleString("sv", { dateStyle: "full" }),
-    "lördag 13 juni 2020",
-  );
 });
 
 Deno.test("day name can be localized", () => {
@@ -126,27 +72,6 @@ Deno.test("can be converted to instant in UTC", () => {
   assertEquals(
     exPlainDate.toUtcInstant(time).toISOString(),
     "2022-02-02T23:59:59.999Z",
-  );
-});
-
-Deno.test("can be converted to instant in local timezone", () => {
-  const exPlainDate = ExPlainDate({ year: 2022, month: 2, day: 2 });
-  const time = { hour: 23, minute: 59, second: 59, millisecond: 999 };
-
-  assertStringIncludes(
-    exPlainDate.toLocalInstant(time).toString(),
-    "Feb 02 2022 23:59:59",
-  );
-});
-
-Deno.test("can be converted to instant in given timezone", () => {
-  const exPlainDate = ExPlainDate({ year: 2022, month: 2, day: 2 });
-  const time = { hour: 23, minute: 59, second: 59, millisecond: 999 };
-
-  // India Standard Time is UTC+5:30
-  assertStringIncludes(
-    exPlainDate.toInstant("IST", time).toISOString(),
-    "2022-02-02T18:29:59.999Z",
   );
 });
 
