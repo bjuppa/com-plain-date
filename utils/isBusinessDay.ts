d1a5fdf83d8fc8921a5442ba517bb850fbda7290
Björn Nilsved <nilsved@gmail.com>
2023-05-03 11:49:07 +0200

    Describe functions

--- utils/isBusinessDay.ts
+++ utils/isBusinessDay.ts
@@ -2,5 +2,6 @@ import { WeekDay } from "../constants.ts";
 import { PlainDatePredicateFn } from "../support/function-signatures.ts";
 import { weekDayNumber } from "./weekDayNumber.ts";
 
+/** Check if a plain date is within Monday to Friday */
 export const isBusinessDay: PlainDatePredicateFn = (plainDate) =>
   weekDayNumber(plainDate) < WeekDay.SATURDAY;
