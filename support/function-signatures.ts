e08336baddf9ab99e39017ab72264c322323df18
Björn Nilsved <nilsved@gmail.com>
2023-05-04 11:14:31 +0200

    Describe function types better

--- support/function-signatures.ts
+++ support/function-signatures.ts
@@ -5,42 +5,42 @@ import {
   SloppyPlainTime,
 } from "./sloppy-types.ts";
 
-/** Function taking a plain-date and returning a boolean */
+/** Describes a function taking a plain-date and returning a boolean */
 export type PlainDatePredicateFn = (plainDate: ComPlainDate) => boolean;
 
-/** Function taking a sloppy plain-date and returning a boolean */
+/** Describes a function taking a sloppy plain-date and returning a boolean */
 export type SloppyPlainDatePredicateFn = (
   plainDate: SloppyPlainDate,
 ) => boolean;
 
-/** Function taking a plain-date and returning a number */
+/** Describes a function taking a plain-date and returning a number */
 export type PlainDateNumberFn = (plainDate: ComPlainDate) => number;
 
-/** Function taking a sloppy plain-date and returning a number */
+/** Describes a function taking a sloppy plain-date and returning a number */
 export type SloppyPlainDateNumberFn = (plainDate: SloppyPlainDate) => number;
 
-/** Function taking a sloppy plain-time and returning a number */
+/** Describes a function taking a sloppy plain-time and returning a number */
 export type SloppyPlainTimeNumberFn = (plainTime: SloppyPlainTime) => number;
 
-/** Function taking a plain-date object and returning the same */
+/** Describes a function taking a plain-date object and returning the same */
 export type PlainDateMapFn = <T extends ComPlainDate>(plainDate: T) => T;
 
-/** Higher order function taking a number and returning a plain-date map function */
+/** Describes a higher order function taking a number and returning a plain-date map function */
 export type HOTimeUnitPlainDateMapFn = (count: number) => PlainDateMapFn;
 
-/** Higher order function for calculating a difference between two objects */
+/** Describes a higher order function for calculating a difference between two objects */
 export type HODifferenceFn<T> = (from: T) => (to: T) => number;
 
-/** Higher order function for calculating a difference between two plain-date objects */
+/** Describes a higher order function for calculating a difference between two plain-date objects */
 export type HOPlainDatesDifferenceFn = HODifferenceFn<ComPlainDate>;
 
-/** Function taking a JS Date object and returning the same */
+/** Describes a function taking a JS Date object and returning the same */
 export type NativeDateMapFn = (instant: Date) => Date;
 
-/** Function taking a sloppy date-time object and returning a JS Date object */
+/** Describes a function taking a sloppy date-time object and returning a JS Date object */
 export type NativeDateFactory = (dateTime: SloppyPlainDateTime) => Date;
 
-/** Function splitting a JS Date object into a tuple of plain-date and plain-time objects */
+/** Describes a function splitting a JS Date object into a tuple of plain-date and plain-time objects */
 export type NativeDateSplitterFn = (instant?: Date) => [
   ComPlainDate,
   {
