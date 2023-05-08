c4cc77e85c2aeacb269214c09de93d71ea5d31fa
Björn Nilsved <nilsved@gmail.com>
2023-05-08 10:43:22 +0200

    Take sloppy dates

--- utils/isLastDayOfMonth.ts
+++ utils/isLastDayOfMonth.ts
@@ -1,8 +1,8 @@
-import { ComPlainDate } from "../PlainDate.ts";
+import { SloppyDate } from "../support/sloppy-types.ts";
 import { daysInMonth } from "./daysInMonth.ts";
 
 /**
- * Check if a plain-date is the last day of its month.
+ * Check if a date is the last day of its month.
  */
-export const isLastDayOfMonth = (date: ComPlainDate) =>
-  date.day === daysInMonth(date);
+export const isLastDayOfMonth = (date: SloppyDate): boolean =>
+  Number(date.day) === daysInMonth(date);
--- utils/isLastDayOfMonth_test.ts
+++ utils/isLastDayOfMonth_test.ts
@@ -1,11 +1,10 @@
 import { isLastDayOfMonth } from "./isLastDayOfMonth.ts";
-import { PlainDate } from "../PlainDate.ts";
 import { assert, assertFalse } from "../dev_deps.ts";
 
 Deno.test("true for last day of month", () => {
-  assert(isLastDayOfMonth(PlainDate({ year: 2023, month: 2, day: 28 })));
+  assert(isLastDayOfMonth({ year: "2023", month: "02", day: "28" }));
 });
 
 Deno.test("false for first day of month", () => {
-  assertFalse(isLastDayOfMonth(PlainDate({ year: 2023, month: 2, day: 1 })));
+  assertFalse(isLastDayOfMonth({ year: 2023, month: 2, day: 1 }));
 });
