8e51b1ca4c29b36d0b45dc0e239ba9ad88b98274
Björn Nilsved <nilsved@gmail.com>
2023-05-08 13:58:03 +0200

    Convert exports to named functions

--- utils/addBusinessDays.ts
+++ utils/addBusinessDays.ts
@@ -10,26 +10,28 @@ import { weekDayNumber } from "./weekDayNumber.ts";
  *
  * Monday-Friday are business days.
  */
-export const addBusinessDays = (businessDays = 0): PlainDateMapFn => (date) => {
-  const fullWeeks = Math.trunc(
-    Math.abs(businessDays) / BUSINESS_DAYS_IN_WEEK,
-  );
-  const fullDays = Math.abs(businessDays) % BUSINESS_DAYS_IN_WEEK;
+export function addBusinessDays(businessDays = 0): PlainDateMapFn {
+  return (date) => {
+    const fullWeeks = Math.trunc(
+      Math.abs(businessDays) / BUSINESS_DAYS_IN_WEEK,
+    );
+    const fullDays = Math.abs(businessDays) % BUSINESS_DAYS_IN_WEEK;
 
-  const dayBase = weekDayNumber(date);
-  const dayRelative = dayBase + Math.sign(businessDays) * fullDays;
-  const weekendDays =
-    (dayBase < WeekDay.SATURDAY && dayRelative > WeekDay.FRIDAY) ||
-      dayRelative < WeekDay.MONDAY
-      ? 2
-      : (dayBase === WeekDay.SATURDAY && dayRelative > WeekDay.FRIDAY) ||
-          (dayBase === WeekDay.SUNDAY && dayRelative < WeekDay.SUNDAY)
-      ? 1
-      : 0;
+    const dayBase = weekDayNumber(date);
+    const dayRelative = dayBase + Math.sign(businessDays) * fullDays;
+    const weekendDays =
+      (dayBase < WeekDay.SATURDAY && dayRelative > WeekDay.FRIDAY) ||
+        dayRelative < WeekDay.MONDAY
+        ? 2
+        : (dayBase === WeekDay.SATURDAY && dayRelative > WeekDay.FRIDAY) ||
+            (dayBase === WeekDay.SUNDAY && dayRelative < WeekDay.SUNDAY)
+        ? 1
+        : 0;
 
-  return addDays(
-    Math.sign(businessDays) *
-      (fullWeeks * DAYS_IN_WEEK +
-        Math.max(0, Math.min(DAYS_IN_WEEK, fullDays + weekendDays))),
-  )(date);
-};
+    return addDays(
+      Math.sign(businessDays) *
+        (fullWeeks * DAYS_IN_WEEK +
+          Math.max(0, Math.min(DAYS_IN_WEEK, fullDays + weekendDays))),
+    )(date);
+  };
+}
--- utils/addDays.ts
+++ utils/addDays.ts
@@ -3,5 +3,6 @@ import { PlainDateMapFn } from "../support/function-signatures.ts";
 /**
  * Get a function curried with a number of days to add to its plain-date arguments.
  */
-export const addDays = (days = 0): PlainDateMapFn => (date) =>
-  date.map((x) => ({ ...x, day: x.day + days }));
+export function addDays(days = 0): PlainDateMapFn {
+  return (date) => date.map((x) => ({ ...x, day: x.day + days }));
+}
