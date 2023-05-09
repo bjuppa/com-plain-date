67cbcab5ddf10e7b8ca64a08951c5604b307f681
Björn Nilsved <nilsved@gmail.com>
2023-05-09 11:32:47 +0200

    Improve docblocs for "first" utilities

--- utils/firstWeekDay.ts
+++ utils/firstWeekDay.ts
@@ -4,7 +4,22 @@ import { addDays } from "./addDays.ts";
 import { weekDayNumber } from "./weekDayNumber.ts";
 
 /**
- * Get a function curried with a weekday to jump to from its plain-date arguments.
+ * Get a function curried with a weekday to jump to
+ * from its plain-date arguments.
+ *
+ * The curried function will return the given date if matching the target weekday.
+ *
+ * @param targetWeekDay ISO weekday number (1-7) starting with Monday
+ * @returns A curried function that operates on plain-dates
+ *
+ * @example
+ * ```ts
+ * // Seeking the first Monday backwards from `date`
+ * const addOneDay = addDays(1);
+ * const subOneWeek = addDays(-7);
+ * const firstMonday = firstWeekDay(WeekDay.MONDAY);
+ * subOneWeek(firstMonday(addOneDay(date)));
+ * ```
  */
 export function firstWeekDay(
   targetWeekDay: WeekDay,
