a467b17cc355341410ac4f566971a6cc0dd50df0
Björn Nilsved <nilsved@gmail.com>
2023-05-09 19:32:51 +0200

    Create build script for npm
    https://github.com/denoland/dnt#setup

---
+++ scripts/build_npm.ts
@@ -0,0 +1,32 @@
+// ex. scripts/build_npm.ts
+import { build, emptyDir } from "https://deno.land/x/dnt/mod.ts";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  shims: {
+    // see JS docs for overview and more options
+    deno: true,
+  },
+  package: {
+    // package.json properties
+    name: "complaindate",
+    version: Deno.args[0],
+    description: "Date-time utilities that keeps timezones on the surface.",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/bjuppa/com-plain-date.git",
+    },
+    bugs: {
+      url: "https://github.com/bjuppa/com-plain-date/issues",
+    },
+  },
+  postBuild() {
+    // steps to run after building and before running the tests
+    Deno.copyFileSync("LICENSE", "npm/LICENSE");
+    Deno.copyFileSync("README.md", "npm/README.md");
+  },
+});
