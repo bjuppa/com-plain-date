612d71d720b71c88b5eb5bfa1a31f8954593bf66
Björn Nilsved <nilsved@gmail.com>
2023-05-17 16:57:10 +0200

    Rework quick-start with pipe

--- examples/quick-start.ts
+++ examples/quick-start.ts
@@ -1,25 +1,47 @@
 import {
-  ExPlainDate,
-  splitDateTime,
+  addDays, // Utility function
+  createInstant, // Utility function
+  firstWeekDay, // Utility function
+  splitDateTime, // Utility function
+  startOfMonth, // Utility function
+  startOfYear, // Utility function
+  WeekDay, // Enum
 } from "https://deno.land/x/complaindate/mod.ts";
 
-// Extract a plain-date and a plain-time representing current wall-time in Sweden
-const [today, time] = splitDateTime("Europe/Stockholm")();
+// Extract a plain-date and a plain-time from any JS `Date`
+// ...and note that doing this requires a timezone
+const [dateA, timeA] = splitDateTime("Europe/Stockholm")(
+  // Sweden is UTC+2 in June, so this `Date` represents 13:30 wall-time there
+  new Date("2023-06-12T13:30:00+02:00"),
+  // Note: Leaving this parameter empty will give you current wall-time (now)
+);
 
 // The plain-date part is an object adhering to the full ComPlainDate interface
-today; // { year: 2023, month: 5, day: 16, iso: "2023-05-16", ...}
+dateA; // { year: 2023, month: 6, day: 12, iso: "2023-06-12", ...}
 
 // The plain-time part is a simple object
-time; // { hour: 14, minute: 40, second: 44, millisecond: 122 }
+timeA; // { hour: 13, minute: 30, second: 0, millisecond: 0 }
 
-/* Note: From here, this example uses `ExPlainDate`, an extended plain-date with many utility functions */
+// Apply a pipeline of operations to get a new plain-date
+const secondTuesdayOfMonth = dateA.pipe(
+  startOfMonth, // Go back to the 1st day of the month
+  firstWeekDay(WeekDay.TUESDAY), // Find the first Tuesday
+  addDays(7), // Move one week ahead
+); // 2023-06-13
 
-// Get the 2nd Tuesday of that month (see, no timezone needed here!)
-const t2 = ExPlainDate(today).startOfMonth().firstTuesday().addDays(7); // 2023-05-09
+// A plain-date can quickly be formatted for a given locale in different formats
+secondTuesdayOfMonth.toLocaleString("en"); // "6/13/23"
+secondTuesdayOfMonth.toLocaleStringFull("sv"); // "tisdag 13 juni 2023"
+secondTuesdayOfMonth.dayNameShort("fr"); // "mar."
 
-// A plain-date can quickly be formatted for a given locale
-t2.toLocaleString("en"); // "5/9/2023"
-t2.toLocaleStringFull("sv"); // "tisdag 9 maj 2023"
+// Utility functions can be called directly with plain-dates
+const dateB = startOfYear(dateA); // 2023-01-01
 
-// Get a JS `Date` with the original time-of-day truncated to the hour
-t2.toInstant("Europe/Stockholm", { hour: time.hour }); // 2023-05-09T12:00:00.000Z
+// Combine a plain-date and a plain-time into a JS `Date`
+// ...and note that doing this requires a timezone
+createInstant(
+  "Europe/London", // The UK is UTC+0 in January
+)({
+  ...dateB, // Spread the plain-date
+  hour: timeA.hour, // Take only the hour from the plain-time, truncating time
+}); // 2023-01-01T13:00:00.000Z
