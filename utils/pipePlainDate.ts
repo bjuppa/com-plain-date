acaf7d8a8dc499e0c1d07625a1b0e63405cb8e2a
Björn Nilsved <nilsved@gmail.com>
2023-06-01 17:34:12 +0200

    Improve docbloc

--- utils/pipePlainDate.ts
+++ utils/pipePlainDate.ts
@@ -3,6 +3,9 @@ import { ComPlainDate } from "../PlainDate.ts";
 /**
  * Get a function curried with a pipeline of functions
  * to apply to its plain-date arguments, from left to right.
+ *
+ * @param fns Functions that take a plain-date and return a plain-date
+ * @returns A function taking a plain-date to pass through the pipeline
  */
 export function pipePlainDate<T extends ComPlainDate>(
   ...fns: Array<(date: T) => T>
