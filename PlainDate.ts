f4478149c97a5b21382513803dae9d84fcffa890
Björn Nilsved <nilsved@gmail.com>
2023-06-01 13:22:41 +0200

    Improve docblocs for map & pipe

--- PlainDate.ts
+++ PlainDate.ts
@@ -1,6 +1,8 @@
-import { SloppyDate, SloppyTime } from "./support/date-time-types.ts";
-import { MonthNumber } from "./constants.ts";
 import { createUtcInstant } from "./utils/createUtcInstant.ts";
+import { SloppyDate, SloppyTime } from "./support/date-time-types.ts";
+// deno-lint-ignore no-unused-vars
+import { PlainDateMapFn } from "./support/function-signatures.ts";
+import { MonthNumber } from "./constants.ts";
 
 export type FormatPlainDateOptions = Omit<
   Intl.DateTimeFormatOptions,
@@ -57,7 +59,12 @@ export interface ComPlainDate {
   constructor: PlainDateFactory<this>;
 
   /**
-   * Create a new plain-date object, applying a pipeline of functions.
+   * Apply a pipeline of functions to this plain-date, from left to right.
+   *
+   * The type {@link PlainDateMapFn} is useful when declaring your own map functions.
+   *
+   * @param {...PlainDateMapFn} fns Functions that take a plain-date and return a plain-date
+   * @returns The output of the last function
    */
   pipe: <T extends ComPlainDate>(
     this: T,
@@ -65,7 +72,10 @@ export interface ComPlainDate {
   ) => T;
 
   /**
-   * Create a new plain-date object, modified by a callback function.
+   * Create a new plain-date object from this one, modified by a function.
+   *
+   * @param f A function that takes a plain-date and returns a sloppy-date
+   * @returns A new plain-date made from the sloppy-date
    */
   map: <T extends ComPlainDate>(
     this: T,
