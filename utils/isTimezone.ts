55de9b43434b0f4a81295f9077856a606e61dd6e
Björn Nilsved <nilsved@gmail.com>
2023-06-13 13:52:08 +0200

    Implement isTimezone util

---
+++ utils/isTimezone.ts
@@ -0,0 +1,11 @@
+/**
+ * Check if a string is a valid timezone in the system.
+ */
+export function isTimezone(timezone: string): boolean {
+  try {
+    Intl.DateTimeFormat(undefined, { timeZone: timezone });
+    return true;
+  } catch (_e) {
+    return false;
+  }
+}
