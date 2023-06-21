4efd67a8950187a85790267c5adfeb9a6cb1edbb
Björn Nilsved <nilsved@gmail.com>
2023-06-21 08:56:24 +0200

    Remove 0 params

--- utils/addDays.ts
+++ utils/addDays.ts
@@ -4,9 +4,9 @@ import { ComPlainDate } from "../PlainDate.ts";
  * Get a function curried with a number of days
  * to add to its plain-date arguments.
  *
- * @param {number} days The number of days to add or subtract
+ * @param days The number of days to add or subtract
  * @returns A curried function that operates on plain-dates
  */
-export function addDays(days = 0): <T extends ComPlainDate>(date: T) => T {
+export function addDays(days: number): <T extends ComPlainDate>(date: T) => T {
   return (date) => date.map((x) => ({ ...x, day: x.day + days }));
 }
--- utils/addYears.ts
+++ utils/addYears.ts
@@ -3,9 +3,11 @@ import { ComPlainDate } from "../PlainDate.ts";
 /**
  * Get a function curried with a number of years to add to its plain-date arguments.
  *
- * @param {number} years The number of years to add or subtract
+ * @param years The number of years to add or subtract
  * @returns A curried function that operates on plain-dates
  */
-export function addYears(years = 0): <T extends ComPlainDate>(date: T) => T {
+export function addYears(
+  years: number,
+): <T extends ComPlainDate>(date: T) => T {
   return (date) => date.map((x) => ({ ...x, year: x.year + years }));
 }
