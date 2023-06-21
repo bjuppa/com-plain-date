cc4681f57d4ff157c96a70cec0842265015c29b8
Björn Nilsved <nilsved@gmail.com>
2023-06-21 15:11:05 +0200

    Try removing default values for object in docblock
    Check https://deno.land/x/complaindate/mod.ts?s=addTime after release

--- utils/addTime.ts
+++ utils/addTime.ts
@@ -5,18 +5,10 @@ import { tallyMilliseconds } from "../support/tallyMilliseconds.ts";
  * Get a function curried with a time duration
  * to add to its native JS `Date` arguments.
  *
- * @param {SloppyTime} duration Object of hour, minute, second & millisecond, where each may be negative
+ * @param time Object of hour, minute, second & millisecond, where each may be negative
  * @returns A curried function that operates on JS `Date` objects
  */
-export function addTime({
-  hour = 0,
-  minute = 0,
-  second = 0,
-  millisecond = 0,
-}: SloppyTime) {
+export function addTime(time: SloppyTime) {
   return (instant: Date): Date =>
-    new Date(
-      instant.valueOf() +
-        tallyMilliseconds({ hour, minute, second, millisecond }),
-    );
+    new Date(instant.valueOf() + tallyMilliseconds(time));
 }
