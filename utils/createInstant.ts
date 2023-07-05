a8bbca71d0dfc6e68e7873165180065812c59466
Björn Nilsved <nilsved@gmail.com>
2023-07-05 09:06:31 +0200

    Reuse 24 h duration object

--- utils/createInstant.ts
+++ utils/createInstant.ts
@@ -98,8 +98,9 @@ const offsetCandidatesMap =
       timeZone: timezone,
       timeZoneName: "longOffset",
     });
-    const future = addTime({ hour: HOURS_IN_DAY })(instant);
-    const past = addTime({ hour: -HOURS_IN_DAY })(instant);
+    const margin = { hour: HOURS_IN_DAY };
+    const future = addTime(margin)(instant);
+    const past = subtractTime(margin)(instant);
     return new Map(
       (prioritizeLaterCandidate ? [future, past] : [past, future])
         .map((instant) =>
