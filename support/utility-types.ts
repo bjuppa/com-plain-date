9af9b8c4a8ae32f020a922d56ab15d1d19e795c4
Björn Nilsved <nilsved@gmail.com>
2023-07-13 09:55:41 +0200

    Implement equality check

---
+++ support/utility-types.ts
@@ -0,0 +1,8 @@
+/**
+ * From https://learn.microsoft.com/en-us/javascript/api/@azure/keyvault-certificates/requireatleastone?view=azure-node-latest
+ */
+export type RequireAtLeastOne<T> = {
+  [K in keyof T]-?:
+    & Required<Pick<T, K>>
+    & Partial<Pick<T, Exclude<keyof T, K>>>;
+}[keyof T];
