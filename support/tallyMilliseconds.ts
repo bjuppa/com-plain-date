16f2aad21f73069d0ed45797db6cfe55ee8dc86f
Björn Nilsved <nilsved@gmail.com>
2023-07-30 14:53:43 +0200

    Pass individual parameters to support functions

--- support/tallyMilliseconds.ts
+++ support/tallyMilliseconds.ts
@@ -1,19 +1,14 @@
 import { MS_IN_HOUR, MS_IN_MINUTE, MS_IN_SECOND } from "../constants.ts";
 
 /**
- * Convert a time duration object to a total number of milliseconds.
+ * Sum durations to a total number of milliseconds.
  */
-export function tallyMilliseconds({
-  hours = 0,
-  minutes = 0,
-  seconds = 0,
-  milliseconds = 0,
-}: {
-  hours?: number | string;
-  minutes?: number | string;
-  seconds?: number | string;
-  milliseconds?: number | string;
-}): number {
+export function tallyMilliseconds(
+  hours: number | string = 0,
+  minutes: number | string = 0,
+  seconds: number | string = 0,
+  milliseconds: number | string = 0,
+): number {
   return Number(hours) * MS_IN_HOUR + Number(minutes) * MS_IN_MINUTE +
     Number(seconds) * MS_IN_SECOND + Number(milliseconds);
 }
--- support/tallyMilliseconds_test.ts
+++ support/tallyMilliseconds_test.ts
@@ -1,25 +1,20 @@
 import { assertEquals } from "../dev_deps.ts";
 import { tallyMilliseconds } from "./tallyMilliseconds.ts";
 
-Deno.test("sums time parts into milliseconds", () => {
+Deno.test("sums time into milliseconds", () => {
   assertEquals(
-    tallyMilliseconds({ hours: 1, minutes: 1, seconds: 1, milliseconds: 1 }),
+    tallyMilliseconds(1, 1, 1, 1),
     (60 * 60 + 60 + 1) * 1000 + 1,
   );
 });
 
-Deno.test("takes negative parts", () => {
+Deno.test("takes negative parameters", () => {
   assertEquals(
-    tallyMilliseconds({
-      hours: -1,
-      minutes: -1,
-      seconds: -1,
-      milliseconds: -1,
-    }),
+    tallyMilliseconds(-1, -1, -1, -1),
     (60 * 60 + 60 + 1) * -1000 - 1,
   );
 });
 
 Deno.test("takes undefined values", () => {
-  assertEquals(tallyMilliseconds({}), 0);
+  assertEquals(tallyMilliseconds(), 0);
 });
