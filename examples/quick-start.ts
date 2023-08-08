24dc2ff1bda834f74b6b325f5bd7712a1eb0ce4f
Björn Nilsved <nilsved@gmail.com>
2023-08-08 14:54:43 +0200

    Reduce comment clutter

--- examples/quick-start.ts
+++ examples/quick-start.ts
@@ -1,4 +1,3 @@
-// Deno users can import directly from deno.land, like this:
 import {
   addDays,
   createInstant,
@@ -19,7 +18,7 @@ const [june6, time1337] = splitDateTime(
 )(
   // Sweden is at UTC+2 in June, so this `Date` represents 13:37 wall-time there
   new Date("2023-06-06T13:37+0200"),
-); // Note: When called without a `Date`, this produces current wall-time (now)
+);
 
 // The plain-date part is an object adhering to the full ComPlainDate interface
 june6; // { year: 2023, month: 6, day: 6, iso: "2023-06-06", ...}
@@ -31,7 +30,6 @@ time1337; // { hour: 13, minute: 37, second: 0, millisecond: 0, ... }
 time1337.toLocaleString("en"); // "1:37 PM"
 
 // Apply any pipeline of operations to get a new plain-date
-// ...free from any hassle involving timezones!
 const midsummersEve = june6.pipe(
   startOfMonth, // Go back to the 1st day of June
   addDays(18), // Move to the first possible midsummer's eve candidate (June 19)
@@ -54,9 +52,8 @@ newYearsDay.toUtcInstant(); // 2023-01-01T00:00:00.000Z
 
 // Combine any shape of local date & time into an "instant", a JS `Date`
 createInstant(
-  // The Wiener Musikverein is at UTC+1 in January
   "Europe/Vienna", // Note: A timezone is required for this operation
 )({
   ...newYearsDay,
-  ...{ hour: 11, minute: 15 },
+  ...{ hour: 11, minute: 15 }, // The Wiener Musikverein is at UTC+1 in January
 }); // 2023-01-01T10:15:00.000Z
